1ca60efbfd6a003783f206bdc5d36584cf72da00
ArloysMacias <arloys.rojas@atg.se>
2020-10-12 03:25:16 +0200

    File upload information modal

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -3,57 +3,48 @@ $(document).ready(function(){
     $("#wizard-picture").change(function(){
         readURL(this);
     });
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#wizardPicturePreview').attr('src', e.target.result).fadeIn('slow');
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
 
 });
 
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
 
-        reader.onload = function (e) {
-            $('#wizardPicturePreview').attr('src', e.target.result).fadeIn('slow');
-        }
-        reader.readAsDataURL(input.files[0]);
-    }
-}
+$(document).ready(function(){
+    $('.modal').modal();
+});
 
-/*
-*   This content is licensed according to the W3C Software License at
-*   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
-*
-*   File:   vertical-slider.js
-*
-*   Desc:   Vertical slider widget that implements ARIA Authoring Practices
-*/
 
-// Create Vertical Slider that contains value, valuemin, valuemax, and valuenow
-var VSlider = function (domNode)  {
+function openModal() {
 
-    this.domNode = domNode;
-    this.railDomNode = domNode.parentNode;
+    //call the specific div (modal)
+    $('#modal').modal('open');
 
-    this.valueDomNode = false;
 
-    this.valueMin = 0;
-    this.valueMax = 100;
-    this.valueNow = 50;
+    var pleaseWait = $('#pleaseWaitDialog');
 
-    this.railHeight = 0;
+    showPleaseWait = function () {
+        pleaseWait.modal('open');
+        //$('#modal').modal('open');
+    };
 
-    this.thumbWidth  = 28;
-    this.thumbHeight = 8;
+    hidePleaseWait = function () {
+        pleaseWait.modal('close');
+        //('#modal').modal('open');
+    };
 
-    this.keyCode = Object.freeze({
-        'left': 37,
-        'up': 38,
-        'right': 39,
-        'down': 40,
-        'pageUp': 33,
-        'pageDown': 34,
-        'end': 35,
-        'home': 36
-    });
+    showPleaseWait();
 };
+
+
+
 
 // Thermometer  https://codepen.io/GeorgePark/pen/oVgGyM
 
@@ -111,3 +102,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Thermometer  https://codepen.io/GeorgePark/pen/oVgGyM
+
+
+
+
